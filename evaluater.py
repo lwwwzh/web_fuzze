96996e693037c95de91722b856ac460f38e60910
lwwwzh <422823976@qq.com>
2017-08-22 18:07:32 +0800

    update dimension_weight module

--- evaluater.py
+++ evaluater.py
@@ -1,24 +1,10 @@
-
 #coding=utf-8
 
 import re
-import time
 
-class Base_res_caler(object):
+class Base_evaluate(object):
 
-    TITLE_PATTERN = re.compile(r'<title>(.*?)</title>')
-    INVALID_KEYWORLD = (u'造成安全威胁', u'疑似黑客攻击', u'创宇盾', u'不合法参数', u'D盾', u'提交的内容包含危险')    
-
-    VALID_PARAM = ('123', 'okff', '1ok2')
-    INVALID_PARAM = ('union select',)
-
-    DIMENSION_WEIGHT = {
-        'status': 3,
-        'length': 3,
-        'title': 3,
-        'delay': 1,
-        'invalid_keyword': 5,
-    }
+    DIMENSION_WEIGHT = {}
     CAL_TOTAL_WEIGHT = 10
     
     def cal_dimension_weight(self, res, *other_args):           
@@ -39,10 +25,6 @@ class Base_res_caler(object):
             var_1 = res_1[dimension]
             var_2 = res_2[dimension]
             
-            #if not var_1 and not var_2:
-                #result = result + 1 * DIMENSION_WEIGHT[dimension]
-                #continue
-        
             cal_meth = getattr(self, 'is_like_%s' % dimension)            
             result = result + cal_meth(var_1, var_2) * self.DIMENSION_WEIGHT[dimension]
         
@@ -50,7 +32,24 @@ class Base_res_caler(object):
         if result > 1:
             result = 1
         return result
+
         
+class Res_evaluate(Base_evaluate):
+
+    TITLE_PATTERN = re.compile(r'<title>(.*?)</title>')
+    INVALID_KEYWORLD = (u'造成安全威胁', u'疑似黑客攻击', u'创宇盾', u'不合法参数', u'D盾', u'提交的内容包含危险')    
+
+    VALID_PARAM = ('123', 'okff', '1ok2')
+    INVALID_PARAM = ('union select',)
+
+    DIMENSION_WEIGHT = {
+        'status': 3,
+        'length': 3,
+        'title': 3,
+        'delay': 1,
+        'invalid_keyword': 5,
+    }
+    
     def cal_status(self, res, *is_delay):
         return res.status_code
     
@@ -58,8 +57,10 @@ class Base_res_caler(object):
         status_1 = int(status_1)
         status_2 = int(status_2)
         
-        if -10 < status_1 - status_2 < 10:
+        if status_1 == status_2:
             return 1
+        elif -10 < status_1 - status_2 < 10:
+            return abs(status_1-status_2)/10.0
         else:
             return 0
     
@@ -67,8 +68,13 @@ class Base_res_caler(object):
         return len(res.text)
     
     def is_like_length(self, length_1, length_2):
-        if length_1*0.85 < length_2 < length_1*1.15:
+        if length_1 < length_2:
+            length_1, length_2 = length_2, length_1
+        
+        if length_1 == length_2 :
             return 1
+        elif length_1*0.85 < length_2 :
+            return float(length_2) / length_1
         else:
             return 0
     
@@ -112,19 +118,31 @@ class Base_res_caler(object):
         if invalid_word_1 and invalid_word_1 == invalid_word_2:
             return 1
         else:
-            return 0  
-        
+            return 0    
+
+
+
 def test():
-    cal = Base_res_caler()
+    cal = Res_evaluate()
     
     import requests
-    res1 = requests.get('http://www.bp.gov.cn/news/?id=56172&a=1')
+    import time
+    
+    url = 'http://www.bp.gov.cn/news/?id=56172'
+    _headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
+    }
+    
+    res1 = requests.get('%s' % url, headers=_headers)
+    print res1.request.headers
+    #print res1.text
     
     time.sleep(1)
-    res3 = requests.get('http://www.bp.gov.cn/news/?id=56172&a=1123')    
+    res3 = requests.get('%s&b=113' % url, headers=_headers)    
     
     time.sleep(1)
-    res2 = requests.get('http://www.bp.gov.cn/news/?id=56172&a=union select')
+    res2 = requests.get('%s&a=union select' % url, headers=_headers)
 
     dw_1 = cal.cal_dimension_weight(res1)
     dw_2 = cal.cal_dimension_weight(res2, False)
@@ -132,8 +150,9 @@ def test():
     print dw_1
     print dw_2
     print dw_3
-    print cal.is_like(dw_1, dw_3)
-    print cal.is_like(dw_1, dw_2)
+    print '1,3', cal.is_like(dw_1, dw_3)
+    print '1,2', cal.is_like(dw_1, dw_2)
+    print '2,3', cal.is_like(dw_2, dw_3)
     
 if __name__ == '__main__':
     test()
